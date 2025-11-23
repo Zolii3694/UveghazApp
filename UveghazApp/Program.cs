27518171cb8e553e27d70ddeb5eb84337b14bc01
PeladyADamn <peladya@gmail.com>
2025-11-23 22:56:23 +0100

    javitva program.cs-be

--- UveghazApp/Program.cs
+++ UveghazApp/Program.cs
@@ -10,8 +10,33 @@ namespace UveghazApp
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            // 3 szenzor példányosítása
+            var homero = new HomersekletSzenzor();
+            var para = new ParatartalomSzenzor();
+            var talaj = new TalajNedvesseg();
 
+            // ESEMÉNYEKRE feliratkozás (objektumszinten)
+            homero.ErtekValtozas += (s, ertek) =>
+                Console.WriteLine($"Hőmérséklet: {ertek:F2} °C");
+
+            para.ErtekValtozas += (s, ertek) =>
+                Console.WriteLine($"Páratartalom: {ertek:F2} %");
+
+            talaj.ErtekValtozas += (s, ertek) =>
+                Console.WriteLine($"Talajnedvesség: {ertek:F2} %");
+
+            // TESZT: 10 mérési ciklus objektumszinten
+            for (int i = 0; i < 10; i++)
+            {
+                homero.ErtekOlvasas();   // generál + event
+                para.ErtekOlvasas();     // generál + event
+                talaj.ErtekOlvasas();    // generál + event
+
+                System.Threading.Thread.Sleep(500);
+            }
+
+            Console.WriteLine("\nTeszt lefutott.");
         }
     }
 }
--- UveghazApp/Uveghaz.cs
+++ UveghazApp/Uveghaz.cs
@@ -104,34 +104,5 @@ namespace UveghazApp
             File.WriteAllText("meresek.json", json);
             Console.WriteLine("JSON file-ba mentes megtortent: meresek.json");
         }
-        static void Main(string[] args)
-        {
-            // 3 szenzor példányosítása
-            var homero = new HomersekletSzenzor();
-            var para = new ParatartalomSzenzor();
-            var talaj = new TalajNedvesseg();
-
-            // ESEMÉNYEKRE feliratkozás (objektumszinten)
-            homero.ErtekValtozas += (s, ertek) =>
-                Console.WriteLine($"Hőmérséklet: {ertek:F2} °C");
-
-            para.ErtekValtozas += (s, ertek) =>
-                Console.WriteLine($"Páratartalom: {ertek:F2} %");
-
-            talaj.ErtekValtozas += (s, ertek) =>
-                Console.WriteLine($"Talajnedvesség: {ertek:F2} %");
-
-            // TESZT: 10 mérési ciklus objektumszinten
-            for (int i = 0; i < 10; i++)
-            {
-                homero.ErtekOlvasas();   // generál + event
-                para.ErtekOlvasas();     // generál + event
-                talaj.ErtekOlvasas();    // generál + event
-
-                System.Threading.Thread.Sleep(500);
-            }
-
-            Console.WriteLine("\nTeszt lefutott.");
-        }
     }
 }
